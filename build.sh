a532b050ec7ecc2411a8a5d870a8f0fc01aea9d0
Björrn Schnabel <bjoern.schnabel09@gmail.com>
2024-02-15 14:01:59 +0100

    trying to add lua-resty-http module, not done

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#docker stop python_container || true && docker rm python_container || true
+docker stop python_container || true && docker rm python_container || true
 docker stop nginx_container || true && docker rm nginx_container || true
 docker volume rm rduinoshared || true
+docker network create -d bridge rduino-bridge || true
 docker volume create --name rduinoshared
-#docker build -t python-script-container -f Dockerfile.python .
-docker build -t nginx-container:dev . -f Dockerfile.nginx
-#docker run --name python_container -p 5000:5000 -v rduinoshared:/rshared python-script-container &
-docker run --name nginx_container -p 80:80 -v rduinoshared:/rshared nginx-container:dev &
+docker build -t python-script-container -f Dockerfile.python .
+docker build -t nginx-container:dev . -f Dockerfile.openresty
+docker run --name python_container --network=rduino-bridge -v rduinoshared:/rshared python-script-container &
+docker run --name nginx_container --network=rduino-bridge -p 127.0.0.5:80:80 -v rduinoshared:/rshared nginx-container:dev &
 read -s -n 1
-#docker stop python_container
+docker stop python_container
 docker stop nginx_container
 exit 0
--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/upload/', methods=['GET', 'POST'])
 def hello():
-    return 'Hello from Python script!'
+	if request.method == 'POST':
+		#filename = request.form.get('upload_file_name')
+		#return '''Successfully uploaded File "{}"'''.format(filename)
+		f = open("/rshared/log.txt", "w")
+		f.write(request.get_data(as_text=True, parse_form_data=True))
+		f.close()
+		return '''incoming request: {}'''.format(request.get_data(as_text=True, parse_form_data=True))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+	app.run(debug=True, host='0.0.0.0')
